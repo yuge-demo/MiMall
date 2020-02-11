9d11b688edfff17a4c8ade2160fb0be2ec88886c
lianyu <1774785796@qq.com>
2020-02-11 20:33:02 +0800

    json mock

--- src/App.vue
+++ src/App.vue
@@ -5,15 +5,29 @@
 </template>
 
 <script>
-import stroge from "./storage/index";
+// import stroge from "./storage/index";
 export default {
   name: "app",
   components: {},
+  data() {
+    return {
+      res: {}
+    };
+  },
   mounted() {
     // stroge.setItem("a", 1);
     // stroge.setItem('user',{a:1})
     // stroge.setItem('abc',{a:1},'user')
-    stroge.clear('a','a')
+    // stroge.clear('a','a')'
+    //本地加载请求json文件的形式
+    // this.axios.get('/mock/user/login.json').then((res)=> {
+    //   this.res = res;
+    // });
+
+    //通过easy-mock平台实现数据mock
+    this.axios.get('/mock/user/login.json').then((res)=> {
+      this.res = res;
+    });
   }
 };
 </script>
