67e376bdde4f355db0d042e88b9f43781318e43b
lianyu <1774785796@qq.com>
2020-03-12 10:15:35 +0800

    axios

--- src/pages/orderConfirm.vue
+++ src/pages/orderConfirm.vue
@@ -1,5 +1,47 @@
 <template>
       <div class="order-confirm">
+            <svg
+                  version="1.1"
+                  xmlns="http://www.w3.org/2000/svg"
+                  xmlns:xlink="http://www.w3.org/1999/xlink"
+                  style="position: absolute; width: 0px; height: 0px; overflow: hidden;"
+            >
+                  <defs>
+                        <symbol id="icon-add" viewBox="0 0 31 32">
+                              <title>add</title>
+                              <path
+                                    d="M30.745 15.152h-14.382v-14.596c0-0.308-0.243-0.557-0.543-0.557s-0.543 0.249-0.543 0.557v14.596h-14.665c-0.3 0-0.543 0.249-0.543 0.557s0.243 0.557 0.543 0.557h14.665v15.177c0 0.307 0.243 0.557 0.543 0.557s0.543-0.249 0.543-0.557v-15.177h14.382c0.3 0 0.543-0.249 0.543-0.557s-0.243-0.557-0.543-0.557z"
+                                    class="path1"
+                              />
+                        </symbol>
+                        <symbol id="icon-del" viewBox="0 0 32 32">
+                              <title>delete</title>
+                              <path
+                                    d="M11.355 4.129v-2.065h9.29v2.065h-9.29zM6.194 29.935v-23.742h19.613v23.742h-19.613zM30.968 4.129h-8.258v-3.097c0-0.569-0.463-1.032-1.032-1.032h-11.355c-0.569 0-1.032 0.463-1.032 1.032v3.097h-8.258c-0.569 0-1.032 0.463-1.032 1.032s0.463 1.032 1.032 1.032h3.097v24.774c0 0.569 0.463 1.032 1.032 1.032h21.677c0.569 0 1.032-0.463 1.032-1.032v-24.774h3.097c0.569 0 1.032-0.463 1.032-1.032s-0.463-1.032-1.032-1.032v0z"
+                                    class="path1"
+                              />
+                              <path
+                                    d="M10.323 9.806c-0.569 0-1.032 0.463-1.032 1.032v14.452c0 0.569 0.463 1.032 1.032 1.032s1.032-0.463 1.032-1.032v-14.452c0-0.569-0.463-1.032-1.032-1.032z"
+                                    class="path2"
+                              />
+                              <path
+                                    d="M16 9.806c-0.569 0-1.032 0.463-1.032 1.032v14.452c0 0.569 0.463 1.032 1.032 1.032s1.032-0.463 1.032-1.032v-14.452c0-0.569-0.463-1.032-1.032-1.032z"
+                                    class="path3"
+                              />
+                              <path
+                                    d="M21.677 9.806c-0.569 0-1.032 0.463-1.032 1.032v14.452c0 0.569 0.463 1.032 1.032 1.032s1.032-0.463 1.032-1.032v-14.452c0-0.569-0.463-1.032-1.032-1.032z"
+                                    class="path4"
+                              />
+                        </symbol>
+                        <symbol id="icon-edit" viewBox="0 0 32 32">
+                              <title>edit</title>
+                              <path
+                                    d="M28.287 8.51l-4.805-4.806 0.831-0.831c0.472-0.472 1.086-0.777 1.564-0.777 0.248 0 0.452 0.082 0.622 0.253l3.143 3.144c0.539 0.54 0.133 1.529-0.524 2.186l-0.831 0.831zM26.805 9.992l-1.138 1.138-4.805-4.806 1.138-1.138 4.805 4.806zM24.186 12.612l-14.758 14.762-4.805-4.806 14.758-14.762 4.805 4.806zM7.379 28.288l-4.892 1.224 1.223-4.894 3.669 3.67zM31.123 4.011l-3.143-3.144c-0.567-0.567-1.294-0.867-2.103-0.867-1.036 0-2.174 0.52-3.045 1.391l-20.429 20.436c-0.135 0.134-0.23 0.302-0.276 0.487l-2.095 8.385c-0.089 0.355 0.017 0.736 0.276 0.995 0.198 0.198 0.461 0.307 0.741 0.307 0.085 0 0.171-0.010 0.254-0.031l8.381-2.096c0.185-0.047 0.354-0.142 0.487-0.276l20.43-20.436c1.409-1.41 2.042-3.632 0.524-5.15v0z"
+                                    class="path1"
+                              />
+                        </symbol>
+                  </defs>
+            </svg>
             <div class="wrapper">
                   <div class="container">
                         <div class="order-box">
@@ -26,6 +68,29 @@
                                                       </a>
                                                 </div>
                                           </div>
+                                          <div
+                                                class="addr-info"
+                                                v-for="(item ,value ) in list"
+                                                :key="value"
+                                          >
+                                                <h2>{{item.receiverName}}</h2>
+                                                <div class="phone">{{item.receiverMobile}}</div>
+                                                <div
+                                                      class="street"
+                                                >{{item.receiverProvince + ' ' + item.receiverCity + '' + item.receiverDistrict + ' ' +item.receiverAddress}}</div>
+                                                <div class="action">
+                                                      <a href="javascript:;" class="fl">
+                                                            <svg class="icon icon-del">
+                                                                  <use xlink:href="#icon-del" />
+                                                            </svg>
+                                                      </a>
+                                                      <a href="javascript:;" class="fr">
+                                                            <svg class="icon icon-edit">
+                                                                  <use xlink:href="#icon-edit" />
+                                                            </svg>
+                                                      </a>
+                                                </div>
+                                          </div>
                                           <div class="addr-add">
                                                 <div class="icon-add"></div>
                                                 <div>添加新地址</div>
@@ -35,21 +100,15 @@
                               <div class="item-good">
                                     <h2>商品</h2>
                                     <ul>
-                                          <li>
+                                          <li v-for="(item ,value) in cartList" :key="value">
                                                 <div class="good-name">
-                                                      <img src="/imgs/item-box-1.png" alt />
-                                                      <span>小米8 6GB 全息幻彩紫 64GB</span>
+                                                      <img v-lazy="item.productMainImage" alt />
+                                                      <span>{{ item.productName+ '' + item.productSubtitle}}</span>
                                                 </div>
-                                                <div class="good-price">1999元x2</div>
-                                                <div class="good-total">1999元</div>
-                                          </li>
-                                          <li>
-                                                <div class="good-name">
-                                                      <img src="/imgs/item-box-2.png" alt />
-                                                      <span>小米8 6GB 全息幻彩紫 64GB</span>
-                                                </div>
-                                                <div class="good-price">1999元x2</div>
-                                                <div class="good-total">1999元</div>
+                                                <div
+                                                      class="good-price"
+                                                >{{item.productPrice}}x{{item.quantity}}</div>
+                                                <div class="good-total">{{item.productTotalPrice}}元</div>
                                           </li>
                                     </ul>
                               </div>
@@ -65,7 +124,7 @@
                               <div class="detail">
                                     <div class="item">
                                           <span class="item-name">商品数量：</span>
-                                          <span class="item-val">1件</span>
+                                          <span class="item-val">{{count}}件</span>
                                     </div>
                                     <div class="item">
                                           <span class="item-name">商品总价：</span>
@@ -81,7 +140,7 @@
                                     </div>
                                     <div class="item">
                                           <span class="item-name">应付总额：</span>
-                                          <span class="item-val">2599元</span>
+                                          <span class="item-val">{{cartTotalPrice}}元</span>
                                     </div>
                               </div>
                               <div class="btn-group">
@@ -103,8 +162,41 @@
 import Model from "./../components/Model";
 export default {
       name: "order-confirm",
+      data() {
+            return {
+                  list: [], //收货地址列表
+                  cartList: [], // 购物车中需要结算的商品列表
+                  cartTotalPrice: 0, //商品总金额
+                  count: 0 //数量
+            };
+      },
       components: {
             Model
+      },
+      mounted() {
+            this.getAddressList();
+            this.getCartList();
+      },
+      methods: {
+            //收货地址
+            getAddressList() {
+                  this.axios.get("/shippings").then(res => {
+                        this.list = res.list;
+                  });
+            },
+            //获取商品列表
+            getCartList() {
+                  this.axios.get("/carts").then(res => {
+                        let list = res.cartProductVoList; //购物车中所有的商品
+                        this.cartTotalPrice = res.cartTotalPrice; //选中的商品总金额
+                        this.cartList = list.filter(
+                              item => item.productSelected
+                        ); //商品列表
+                        this.cartList.map(item => {
+                              this.count += item.quantity;
+                        });
+                  });
+            }
       }
 };
 </script>
@@ -162,13 +254,13 @@ export default {
                                                 width: 20px;
                                                 height: 20px;
                                                 fill: #666666;
-                                                visibility: hidden;
+                                                vertical-align: middle;
                                                 &:hover {
                                                       fill: #ff6700;
                                                 }
                                           }
                                           &.checked {
-                                                border: 1px solid #ff6700；;
+                                                border: 1px solid #ff6700;
                                           }
                                     }
                               }
@@ -211,7 +303,6 @@ export default {
                                     img {
                                           width: 30px;
                                           height: 30px;
-                                          background-size: contain;
                                           vertical-align: middle;
                                     }
                               }
